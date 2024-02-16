d7174e792dbaa885e89f0e53d82a57de1aeb870c
theo garcia <theogarcia16@gmail.com>
2024-02-16 00:00:36 -0300

    se termino

--- ecomerce/src/App.jsx
+++ ecomerce/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar/navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <ItemListContainer greeting={"Bienvenidos"}/>
     </div>
   )
 }
